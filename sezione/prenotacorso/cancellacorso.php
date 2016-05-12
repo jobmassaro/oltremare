e7a6b14b7fbadc8ac1e838112a3da72f7bd442ca
root <jobmassarol@gmail.com>
2016-05-12 19:04:17 +0200

    step

--- sezione/prenotacorso/cancellacorso.php
+++ sezione/prenotacorso/cancellacorso.php
@@ -30,6 +30,7 @@ $result = $mysqli->query($sql);
        $arr[] = $row;
    }
    unlink("sezione/prenotacorso/utente.json");
+   unlink("sezione/prenotacorso/corsi_prenotati.json");
    $myfile = fopen("sezione/prenotacorso/corsi_prenotati.json", "w") or die("Unable to open file!");
    $txt =json_encode($arr);
    fwrite($myfile, $txt);
