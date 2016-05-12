d175126637e4f9899adbb9d6d1e841961078e68d
root <jobmassarol@gmail.com>
2016-05-12 19:31:51 +0200

    step

--- sezione/corsi/cancellaCorso.php
+++ sezione/corsi/cancellaCorso.php
@@ -18,7 +18,7 @@
 $data = json_decode(file_get_contents("php://input")); 
 $id = mysqli_real_escape_string($mysqli, $data->id);
 
-$sql = "DELETE FROM cv_formazione_oltremare WHERE id = " .$id ;
+$sql = "DELETE FROM cv_formazione WHERE id = " .$id ;
 
 $result = $mysqli->query($sql);
   $arr = array();
